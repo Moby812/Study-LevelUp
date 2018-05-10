2b56197c54670a6d00c627a8df9c605bb3622cfd
Moby812 <moby812@gmail.com>
2018-05-10 19:57:04 +0300

    первые 20 элементов последовательности 2 4 8 16

--- src/Lesson_2/Task_9.java
+++ src/Lesson_2/Task_9.java
@@ -2,3 +2,14 @@
 9. Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….
 */
 
+package Lesson_2;
+
+public class Task_9 {
+    public static void main(String[] args) {
+
+        int count=0;
+        for (int i=2; count < 20; i *= 2, count+=1)
+            System.out.println(i);
+        System.out.println("Было выведено "+ count + " значений");
+    }
+}

733e149e9d76e8a7dbddd1dfdd7241e82ac2bfdd
Paramonov Pavel <moby812@gmail.com>
2018-05-10 17:20:12 +0300

    Первые 55 элементов последовательности 1 3 5 7 9..

--- src/Lesson_2/Task_7.java
+++ src/Lesson_2/Task_7.java
@@ -2,3 +2,15 @@
 7. Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….
 */
 
+package Lesson_2;
+
+public class Task_7 {
+    public static void main(String[] args) {
+
+        int count=0;
+        for (int i=1; count < 55; i += 2, count+=1)
+            System.out.println(i);
+        System.out.println("Было выведено "+ count + " значений");
+
+    }
+}

77b6c2d0b3e9cb63e3e2d7a3eae2682ffbe3cbb3
Paramonov Pavel <moby812@gmail.com>
2018-05-10 17:35:25 +0300

    все неотриц. элементы последовательности 90 85 80

--- src/Lesson_2/Task_8.java
+++ src/Lesson_2/Task_8.java
@@ -2,3 +2,13 @@
 8. Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….
 */
 
+package Lesson_2;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        int count=0;
+        for (int i=90; i >= 0; i -= 5, count+=1)
+            System.out.println(i);
+        System.out.println("Было выведено "+ count + " значений");
+    }
+}

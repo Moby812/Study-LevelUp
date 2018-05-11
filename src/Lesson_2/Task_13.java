d0c0e97231ed92b33d3bdf51785f15845e47184f
Paramonov Pavel <moby812@gmail.com>
2018-05-11 15:02:40 +0300

    первые 11 членов последовательности Фибоначчи

--- src/Lesson_2/Task_13.java
+++ src/Lesson_2/Task_13.java
@@ -6,4 +6,15 @@
 package Lesson_2;
 
 public class Task_13 {
+    public static void main(String args[]) {
+        int x = 11;                             //сколько вывести чисел Фибоначчи = 11
+        int fib_1= 0, fib_2= 1, fib_3;
+        System.out.print(fib_1 +" "+ fib_2 +" ");
+        for (int i = 3; i <= x; i++) {
+            fib_3 = fib_1+fib_2;
+            System.out.print(fib_3 +" ");
+            fib_1 = fib_2;
+            fib_2 = fib_3;
+        }
+    }
 }
